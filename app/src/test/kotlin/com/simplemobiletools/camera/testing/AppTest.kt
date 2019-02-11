68e659ca0bef840495e7330948bc78bd032c886b
DanielPri <privorotskyd@gmail.com>
2019-02-11 15:50:11 -0500

    #12 complying to ktlint standard

--- app/src/test/kotlin/com/simplemobiletools/camera/testing/AppTest.kt
+++ app/src/test/kotlin/com/simplemobiletools/camera/testing/AppTest.kt
@@ -4,18 +4,17 @@ import com.simplemobiletools.camera.activities.MainActivity
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
-import org.mockito.Mockito.`when` as _when
+// import org.mockito.Mockito.`when` as _when
 
-//Useful link to mockito using kotlin
-//https://antonioleiva.com/unit-tests-android-kotlin/
+// Useful link to mockito using kotlin
+// https://antonioleiva.com/unit-tests-android-kotlin/
 
 class AppTest {
 
     @Test
-    fun testJunit(){
+    fun testJunit() {
         val main = mock(MainActivity::class.java)
         main.setIsCameraAvailable(false)
         verify(main).setIsCameraAvailable(false)
     }
 }
-
