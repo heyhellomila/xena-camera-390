9c31385bc2861f39eb9f7847c8bc661a77b04598
DanielPri <privorotskyd@gmail.com>
2019-02-11 15:37:05 -0500

    Junit4 and Mockito-2 are now functional in the test directory

--- app/src/test/kotlin/com/simplemobiletools/camera/testing/AppTest.kt
+++ app/src/test/kotlin/com/simplemobiletools/camera/testing/AppTest.kt
@@ -1,9 +1,21 @@
 package com.simplemobiletools.camera.testing
 
+import com.simplemobiletools.camera.activities.MainActivity
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when` as _when
+
+//Useful link to mockito using kotlin
+//https://antonioleiva.com/unit-tests-android-kotlin/
+
 class AppTest {
 
-
+    @Test
     fun testJunit(){
-        assert(true)
+        val main = mock(MainActivity::class.java)
+        main.setIsCameraAvailable(false)
+        verify(main).setIsCameraAvailable(false)
     }
 }
+
