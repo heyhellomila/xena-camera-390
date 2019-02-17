1dd40ae746581d6bc1068f00d983bcb7fa2f60fd
DanielPri <privorotskyd@gmail.com>
2019-02-16 20:54:40 -0500

    Added unit test that successfully tests the onEditCompleted() method of FilterActivity

--- app/src/test/kotlin/com/simplemobiletools/camera/testing/FilterTest.kt
+++ app/src/test/kotlin/com/simplemobiletools/camera/testing/FilterTest.kt
@@ -3,21 +3,21 @@ package com.simplemobiletools.camera.testing
 import android.graphics.Bitmap
 import com.simplemobiletools.camera.activities.FilterActivity
 import com.zomato.photofilters.imageprocessors.Filter
-import com.zomato.photofilters.imageprocessors.SubFilter
 import org.junit.Before
-import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.ArgumentMatchers
 import org.mockito.Mock
-import org.mockito.Mockito.`when`
-import org.mockito.Mockito.any
+import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations
+import org.mockito.Spy
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
 class FilterTest {
 
-    var filterActivity = FilterActivity()
+    @Spy
+    lateinit var filterActivity : FilterActivity
 
     @Mock
     lateinit var image : Bitmap
@@ -25,26 +25,26 @@ class FilterTest {
     @Mock
     lateinit var filter : Filter
 
-    @Mock
-    lateinit var subFilter : SubFilter
-
-    @Mock
-    lateinit var contrastSubFilter : SubFilter
-
     @Before
     fun initiateFilter() {
-        `when`(filter.processFilter(image)).thenReturn(image)
-        `when`(image.copy(Bitmap.Config.ARGB_8888, true)).thenReturn(image)
-        `when`(subFilter.process(ArgumentMatchers.any())).thenReturn(image)
-        `when`(contrastSubFilter.process(ArgumentMatchers.any())).thenReturn(image)
+        MockitoAnnotations.initMocks(this)
     }
 
     @Test
     fun testOnEditCompleted(){
-        filterActivity.filteredImage = image
-        filterActivity.onEditCompleted()
-        assertEquals(1,1)
-    }
 
+        // initialisation for this test
+        filterActivity.filteredImage = image
+        `when`(filter.processFilter(image)).thenReturn(image)
+        `when`(image.copy(Bitmap.Config.ARGB_8888, true)).thenReturn(image)
+        doReturn(filter).`when`(filterActivity).createFilter()
+        `when`(filter.processFilter(ArgumentMatchers.any())).thenReturn(image)
+
+        // Test
+        filterActivity.onEditCompleted()
+
+        // Assertions and/or verifications
+        verify(filterActivity).onEditCompleted()
+    }
 
 }
