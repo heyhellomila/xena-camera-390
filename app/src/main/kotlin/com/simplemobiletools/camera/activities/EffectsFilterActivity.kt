3266bd096036b82e2b2f58fc8672194468fd0919
Mickey P <anamika.pancholy@gmail.com>
2019-03-07 13:44:05 -0500

    Revert "Testing open filter menu as popup with button"
    This reverts commit fd6bb95e0be306e997a463862b2be60a7a1716b0.

--- app/src/main/kotlin/com/simplemobiletools/camera/activities/EffectsFilterActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/camera/activities/EffectsFilterActivity.kt
@@ -29,10 +29,6 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
-import android.view.View
-import android.widget.Button
-import android.widget.PopupMenu
-import kotlinx.android.synthetic.main.activity_effectsfilter.view.*
 
 class EffectsFilterActivity : Activity(), GLSurfaceView.Renderer {
 
@@ -47,7 +43,6 @@ class EffectsFilterActivity : Activity(), GLSurfaceView.Renderer {
     internal var mCurrentEffect: Int = 0
     // @Volatile
     private var saveFrame: Boolean = false
-    private var effectsButton: Button = findViewById(R.id.openeffects)
 
     fun setCurrentEffect(effect: Int) {
         mCurrentEffect = effect
@@ -66,30 +61,6 @@ class EffectsFilterActivity : Activity(), GLSurfaceView.Renderer {
         mEffectView!!.setRenderer(this)
         mEffectView!!.renderMode = GLSurfaceView.RENDERMODE_WHEN_DIRTY
         mCurrentEffect = R.id.none
-
-
-        /*val clickListener = View.OnClickListener { view ->
-            when (view.id) {
-                R.id.openeffects -> {
-                    showPopup(view)
-                }
-            }
-        }
-        effectsButton.setOnClickListener(clickListener)*/
-
-    }
-
-    fun showPopUp(view: View) {
-        val popupMenu = PopupMenu(this, view)
-        val inflater = popupMenu.menuInflater
-        inflater.inflate(R.menu.effectsfilter, popupMenu.menu)
-        popupMenu.show()
-
-        popupMenu.setOnMenuItemClickListener { item: MenuItem ->
-            setCurrentEffect(item.itemId)
-            mEffectView!!.requestRender()
-true
-        }
     }
 
     private fun loadTextures() {
@@ -306,7 +277,6 @@ true
     override fun onSurfaceCreated(gl: GL10, config: EGLConfig) {
     }
 
-/*
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.effectsfilter, menu)
@@ -318,6 +288,4 @@ true
         mEffectView!!.requestRender()
         return true
     }
-*/
-
 }
