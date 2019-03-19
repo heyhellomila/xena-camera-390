d38c177507336325e597992b43071f9a4e30bf98
Jamal G <jamal.g16@hotmail.com>
2019-03-19 19:27:19 -0400

    [#42 and #47] Comply with ktlint

--- app/src/test/kotlin/com/simplemobiletools/camera/testing/FilterTest.kt
+++ app/src/test/kotlin/com/simplemobiletools/camera/testing/FilterTest.kt
@@ -1,11 +1,7 @@
 package com.simplemobiletools.camera.testing
 
 import com.simplemobiletools.camera.activities.EffectsFilterActivity
-import com.simplemobiletools.camera.helpers.GLToolbox
 import org.junit.Test
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
-import javax.microedition.khronos.opengles.GL10
 import org.mockito.Mockito.`when` as _when
 
 // Useful link to mockito using kotlin
@@ -16,8 +12,8 @@ class FilterTest {
     // Tests setting an effect.
     @Test
     fun testSetCurrentEffect() {
-        val filterActivity = EffectsFilterActivity();
+        val filterActivity = EffectsFilterActivity()
         filterActivity.setCurrentEffect(1)
-        assert(filterActivity.mCurrentEffect==1)
+        assert(filterActivity.mCurrentEffect == 1)
     }
 }
