ef1806a33b17c1199fa9b59df0df3bac36f76bd4
Mickey P <anamika.pancholy@gmail.com>
2019-04-21 18:29:02 -0400

    [#47] Removed extraneous testing variable from MainActivity

--- app/src/main/kotlin/com/simplemobiletools/camera/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/camera/activities/MainActivity.kt
@@ -81,7 +81,6 @@ class MainActivity : SimpleActivity(), PhotoProcessor.MediaSavedListener, Recogn
     }
 
     //voice activation testing variables
-    var handleToggleVoiceCalled: Int = 0
     var tryTakePictureCalled: Int = 0
 
 
@@ -382,7 +381,6 @@ class MainActivity : SimpleActivity(), PhotoProcessor.MediaSavedListener, Recogn
 
     // This method will handle voice activation by first requesting microphone permissions.
     private fun handleToggleVoice() {
-        handleToggleVoiceCalled += 1
         handlePermission(PERMISSION_RECORD_AUDIO) {
             if (it) {
                 toggleVoice()
