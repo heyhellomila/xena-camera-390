d9d439d0e3298ab47ef6a71f561b0010e502a8e5
Mickey P <anamika.pancholy@gmail.com>
2019-04-21 18:21:25 -0400

    [#47] Removed useless test from VoiceActivationTest

--- app/src/test/kotlin/com/simplemobiletools/camera/testing/VoiceActivationTest.kt
+++ app/src/test/kotlin/com/simplemobiletools/camera/testing/VoiceActivationTest.kt
@@ -5,29 +5,14 @@ import org.junit.Before
 import org.junit.Test
 
 class VoiceActivationTest {
-    var main: MainActivity? = null
-
-    @Before
-    fun setup() {
-        main = MainActivity()
-    }
-
-    // Check that Voice Activation Handler is run when the toggle button is pressed
-    @Test
-    fun testHandleToggleVoice() {
-        var originalHandleCallNumber = main!!.handleToggleVoiceCalled
-        // Need something here that calls and runs main.handleToggleVoice() -->  toggle_voice.performClick()?
-        assert(main!!.handleToggleVoiceCalled == originalHandleCallNumber+1)
-    }
-
-
 
     // Check that a picture is taken when keyword is detected
     @Test
     fun testTakePicture() {
-        var originalTryTakePictureCallNumber = main!!.tryTakePictureCalled
-        main!!.onKeywordDetected()
-        assert(main!!.tryTakePictureCalled == originalTryTakePictureCallNumber+1)
+        val main = MainActivity()
+        var originalTryTakePictureCallNumber = main.tryTakePictureCalled
+        main.onKeywordDetected()
+        assert(main.tryTakePictureCalled == originalTryTakePictureCallNumber+1)
     }
 
 }
