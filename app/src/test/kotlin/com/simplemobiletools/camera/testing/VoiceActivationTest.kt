b0f5d2ee87cf6ecec8e77d2e0e57d6481deceb5a
Mickey P <anamika.pancholy@gmail.com>
2019-04-21 00:29:08 -0400

    [#47] Reformatted Voice Activation Unit Tests (Still failing at runtime)

--- app/src/test/kotlin/com/simplemobiletools/camera/testing/VoiceActivationTest.kt
+++ app/src/test/kotlin/com/simplemobiletools/camera/testing/VoiceActivationTest.kt
@@ -1,24 +1,27 @@
 package com.simplemobiletools.camera.testing
 
-import android.widget.Button
 import com.simplemobiletools.camera.activities.MainActivity
-//import com.simplemobiletools.camera.R
+import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
 
 
 class VoiceActivationTest {
+    var main: MainActivity? = null
 
+    @Before
+    fun setup() {
+        main = MainActivity()
+    }
 
     // Check that Voice Activation Handler is run when the toggle button is pressed
     @Test
     fun testHandleToggleVoice() {
-        val main = MainActivity()
-        var originalHandleCallNumber = main.handleToggleVoiceCalled
+        var originalHandleCallNumber = main!!.handleToggleVoiceCalled
         // var originalToggleCallNumber = main.toggleVoiceCalled
-        //  R.id.toggle_voice.performClick()
-        assert(main.handleToggleVoiceCalled == originalHandleCallNumber+1)
+        // R.id.toggle_voice.performClick()
+        assert(main!!.handleToggleVoiceCalled == originalHandleCallNumber+1)
         //assert(main.toggleVoiceCalled == originalToggleCallNumber+1)
     }
 
@@ -27,10 +30,9 @@ class VoiceActivationTest {
     // Check that a picture is taken
     @Test
     fun testTakePicture() {
-        val main = MainActivity()
-        var originalTryTakePictureCallNumber = main.tryTakePictureCalled
-        main.onKeywordDetected()
-        assert(main.tryTakePictureCalled == originalTryTakePictureCallNumber+1)
+        var originalTryTakePictureCallNumber = main!!.tryTakePictureCalled
+        main!!.onKeywordDetected()
+        assert(main!!.tryTakePictureCalled == originalTryTakePictureCallNumber+1)
     }
 
 }
