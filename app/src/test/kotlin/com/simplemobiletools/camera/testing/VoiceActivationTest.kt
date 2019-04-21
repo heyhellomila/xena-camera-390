26d84d97c9e4d32095d5f7cf84fceea3ce2c3d37
Mickey P <anamika.pancholy@gmail.com>
2019-04-21 00:11:24 -0400

    [#47] Voice Activation Unit Tests Build (still fail on runtime)

--- app/src/test/kotlin/com/simplemobiletools/camera/testing/VoiceActivationTest.kt
+++ app/src/test/kotlin/com/simplemobiletools/camera/testing/VoiceActivationTest.kt
@@ -1,6 +1,8 @@
 package com.simplemobiletools.camera.testing
 
+import android.widget.Button
 import com.simplemobiletools.camera.activities.MainActivity
+//import com.simplemobiletools.camera.R
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
@@ -8,24 +10,24 @@ import org.mockito.Mockito.verify
 
 class VoiceActivationTest {
 
-/*
+
     // Check that Voice Activation Handler is run when the toggle button is pressed
     @Test
     fun testHandleToggleVoice() {
-        val main = MainActivity::class.java
+        val main = MainActivity()
         var originalHandleCallNumber = main.handleToggleVoiceCalled
         // var originalToggleCallNumber = main.toggleVoiceCalled
-        ((Button) R.id.toggle_voice).performClick()
+        //  R.id.toggle_voice.performClick()
         assert(main.handleToggleVoiceCalled == originalHandleCallNumber+1)
         //assert(main.toggleVoiceCalled == originalToggleCallNumber+1)
     }
-*/
+
 
 
     // Check that a picture is taken
     @Test
     fun testTakePicture() {
-        val main = MainActivity::class.java
+        val main = MainActivity()
         var originalTryTakePictureCallNumber = main.tryTakePictureCalled
         main.onKeywordDetected()
         assert(main.tryTakePictureCalled == originalTryTakePictureCallNumber+1)
