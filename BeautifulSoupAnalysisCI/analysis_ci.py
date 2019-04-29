56b649574ef10130b7cba71317aa35957bc238e2
Mila Roisin <mila.roisin@gmail.com>
2019-04-28 20:11:25 -0400

    [#60] File format correction edited for text parser

--- BeautifulSoupAnalysisCI/analysis_ci.py
+++ BeautifulSoupAnalysisCI/analysis_ci.py
@@ -1,10 +1,11 @@
 
-## for when parsing raw data from a txt file
+## for when parsing raw data from a txt file -- alternative solution
+
 import re #regex
 
 log = # open the raw log as a string
 
-ex = re.compile(" > VoiceActivationTest.kt\w* FAILED\\r\\n.+?(?=\\r\\n)")
+ex = re.compile(" > VoiceActivationTest\w* FAILED\\r\\n.+?(?=\\r\\n)")
 x = ex.findall(log)
 
 for match in x:
